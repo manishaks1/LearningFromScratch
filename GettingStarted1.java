88aef1eed9c6452a3f47a308b81d0f81d03af02e
Manisha Kumari <krimanisha.1707@gmail.com>
2024-07-01 12:36:55 +0530

    Create GettingStarted1.java

---
+++ GettingStarted1.java
@@ -0,0 +1,17 @@
+/*
+Hello
+
+World !
+*/
+
+import java.lang.*;
+import java.util.*;
+
+public class GettingStarted1{
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println();
+        System.out.println("World !");
+        
+    }
+}
