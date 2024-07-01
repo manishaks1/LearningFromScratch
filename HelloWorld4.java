bcaca844dde6da5cf6064cc436fbcad545668258
Manisha Kumari <krimanisha.1707@gmail.com>
2024-07-01 12:35:36 +0530

    Create HelloWorld4.java

---
+++ HelloWorld4.java
@@ -0,0 +1,15 @@
+/*
+Hello 40
+50 World !
+*/
+
+import java.lang.*;
+import java.util.*;
+
+public class HelloWorld4{
+    public static void main(String[] args) {
+        System.out.println("Hello 40");
+        System.out.println("50 World !");
+        
+    }
+}
